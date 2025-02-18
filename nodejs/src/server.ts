57bfbb3fd6c362c6537edefd1eeef6a226ea1fae
Pedro Henrique Bergamo <pedroh.bergamo20@gmail.com>
2025-02-18 16:17:32 -0300

    🎉 NodeJS Aula 01 - Criação e configuração inicial do projeto

---
+++ nodejs/src/server.ts
@@ -0,0 +1,41 @@
+import { fastifyCors } from "@fastify/cors";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import fastify from "fastify";
+import {
+	type ZodTypeProvider,
+	jsonSchemaTransform,
+	serializerCompiler,
+	validatorCompiler,
+} from "fastify-type-provider-zod";
+import { env } from "./env";
+import { subscriberToEventRoute } from "./routes/subscriber-to-event-route";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifyCors, {
+	origin: true,
+});
+
+app.register(fastifySwagger, {
+	openapi: {
+		info: {
+			title: "NLW Connect",
+			version: "1.0.0",
+		},
+	},
+	transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUi, {
+	routePrefix: "/docs",
+});
+
+app.register(subscriberToEventRoute);
+
+app.listen({ port: env.PORT }).then(() => {
+	console.log(`🚀 HTTP server running on port ${env.PORT}`);
+});
